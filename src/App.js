55d2e34e13571ee53811647d90608f619ba34fc0
paridhikhaitan <paridhi4699@gmail.com>
2020-08-24 04:07:43 +0530

    finished the styles

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ setConfiguration({ gutterWidth: 40 });
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
-      <h2>Hello World</h2>
-      <h3>Hello World</h3>
-      <h4>Hello World</h4>
-      <h5>Hello World</h5>
-      <h6>Hello World</h6>
-      <p>Hello World</p>
+      <h1 className="darkest-background-section">{"<h1> "}Hello World</h1>
+      <h2 className="mid-background-section "> {"<h2> "}Hello World</h2>
+      <h3 className="lightest-background-section"> {"<h3> "}Hello World</h3>
+      <h4 className="white-background-section"> {"<h4> "}Hello World</h4>
+      <h5> {"<h5> "}Hello World</h5>
+      <h6> {"<h6> "}Hello World</h6>
+      <p> {"<p> "}Hello World</p>
     </div>
   );
 }
