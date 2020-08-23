79f2d0b62ac9c987e73c884599c329bce9fff826
paridhikhaitan <paridhi4699@gmail.com>
2020-08-24 04:11:08 +0530

    added everything

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { setConfiguration } from "react-grid-system";
+import { setConfiguration, Row, Col, Container } from "react-grid-system";
 import "./styles/App.scss";
 
 setConfiguration({ gutterWidth: 40 });
@@ -8,12 +8,34 @@ function App() {
   return (
     <div>
       <h1 className="darkest-background-section">{"<h1> "}Hello World</h1>
-      <h2 className="mid-background-section "> {"<h2> "}Hello World</h2>
+      <h2 className="mid-background-section">{"<h2> "}Hello World</h2>
       <h3 className="lightest-background-section"> {"<h3> "}Hello World</h3>
       <h4 className="white-background-section"> {"<h4> "}Hello World</h4>
       <h5> {"<h5> "}Hello World</h5>
       <h6> {"<h6> "}Hello World</h6>
       <p> {"<p> "}Hello World</p>
+      <Container>
+        <Row>
+          <Col className="darkest-background-section" sm={6} md={4}>
+            <h1>THESE</h1>
+          </Col>
+          <Col className="mid-background-section " sm={6} md={4}>
+            <h2> ARE</h2>
+          </Col>
+          <Col className="white-background-section" sm={6} md={4}>
+            <h3>GRID</h3>
+          </Col>
+          <Col className="mid-background-section " sm={6} md={4}>
+            <h4> COLUMNS</h4>
+          </Col>
+          <Col className="lightest-background-section" sm={6} md={4}>
+            <h5>FOR</h5>
+          </Col>
+          <Col className="mid-background-section " sm={6} md={4}>
+            <h6> DEMO</h6>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
