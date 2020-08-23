5dc39b75b1262f776d44fc8baa3297701472f277
paridhikhaitan <paridhi4699@gmail.com>
2020-08-24 03:51:42 +0530

    completed the typography part

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { setConfiguration } from "react-grid-system";
+import "./styles/App.scss";
 
-
-setConfiguration({gutterWidth : 40})
+setConfiguration({ gutterWidth: 40 });
 
 function App() {
   return (
     <div>
       <h1>Hello World</h1>
+      <h2>Hello World</h2>
+      <h3>Hello World</h3>
+      <h4>Hello World</h4>
+      <h5>Hello World</h5>
+      <h6>Hello World</h6>
+      <p>Hello World</p>
     </div>
   );
 }
