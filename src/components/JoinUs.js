de974312045758c7e9cb423ddb9504606c470e57
Sravya Balasa <sravya.balasa@gmail.com>
2020-08-26 20:34:37 -0700

    centered quote and added half highlight

--- src/components/JoinUs.js
+++ src/components/JoinUs.js
@@ -11,9 +11,15 @@ class JoinUs extends React.Component{
                     <Row>
                         <Col>
                             <h1>Join Us</h1>
-                            <div id="join-us-quote-icon">
-                                <ImQuotesLeft/>
-                                <p id="join-us-quote">if you teach <strong id="quote-bold">one girl</strong> how to code, she will <strong id="quote-bold">teach three</strong></p>
+                        </Col>
+                    </Row>
+                    <Row justify="center">
+                        <Col xs={10} md={9}>
+                            <div id="join-us-body">
+                                <div id="join-us-quote-icon">
+                                    <ImQuotesLeft/>
+                                </div>
+                                <p id="join-us-quote">if you teach <span id="quote-bold">&nbsp;one girl&nbsp;</span> how to code, she will <span id="quote-bold">&nbsp;teach three&nbsp;</span>.</p>
                             </div>
                         </Col>
                     </Row>
