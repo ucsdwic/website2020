0f77e2e07e172f31dd6df394ec93a966086753bb
paridhikhaitan <paridhi4699@gmail.com>
2020-09-05 20:22:17 +0530

    added the buttons thing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { setConfiguration, Row, Col, Container } from "react-grid-system";
 import "./styles/App.scss";
 import JoinUs from "./components/JoinUs";
-import AboutUs from "./components/AboutUs"
+import AboutUs from "./components/AboutUs";
 import MeetTheTeam from "./pages/MeetTheTeam";
 import GetInvolved from "./components/getInvolved.js";
 import ContactUs from "./components/contactUs.js";
@@ -15,14 +15,12 @@ setConfiguration({ gutterWidth: 80 });
 function App() {
   return (
     <div>
-      <AboutUs/>
-      <MeetTheTeam/>
+      <AboutUs />
+      <MeetTheTeam />
       <GetInvolved />
-      <Sponsors/>
-      <JoinUs/>
-      <ContactUs/>
-      <Button type="secondary" text = "Sponsor Us" toLink = "https://google.com"/>
-      <Button type="primary" text = "Sponsor Us" toLink = "https://google.com" />
+      <Sponsors />
+      <JoinUs />
+      <ContactUs />
     </div>
   );
 }
