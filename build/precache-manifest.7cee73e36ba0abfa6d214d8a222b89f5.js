559373b5890b8b116704acca4bf419bfdb302dee
Jaida Day <jday1418@gmail.com>
2022-06-01 22:35:20 -0700

    updated home background photo

---
+++ build/precache-manifest.7cee73e36ba0abfa6d214d8a222b89f5.js
@@ -0,0 +1,230 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "029d2765650177744342aeb5782ce5b2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "503ec405f65a7c30dbc7",
+    "url": "./static/css/2.e30d590b.chunk.css"
+  },
+  {
+    "revision": "d008b726accb6de14919",
+    "url": "./static/css/main.b1a0d388.chunk.css"
+  },
+  {
+    "revision": "503ec405f65a7c30dbc7",
+    "url": "./static/js/2.b01e8371.chunk.js"
+  },
+  {
+    "revision": "595a6e156d8cc00e3d05b545e34ea23c",
+    "url": "./static/js/2.b01e8371.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d008b726accb6de14919",
+    "url": "./static/js/main.97d95d1f.chunk.js"
+  },
+  {
+    "revision": "ebb8d834bada9d2a1e15",
+    "url": "./static/js/runtime-main.c9b3e07e.js"
+  },
+  {
+    "revision": "8872d2c829f5c65898ed6cc85d364bb5",
+    "url": "./static/media/2021-2022.8872d2c8.jpg"
+  },
+  {
+    "revision": "1d0d8f7a7c46082a10c1747e381a244d",
+    "url": "./static/media/Kollektif-Bold.1d0d8f7a.svg"
+  },
+  {
+    "revision": "64142c01735cb9bb6defdcf7a9786364",
+    "url": "./static/media/Kollektif-Bold.64142c01.woff"
+  },
+  {
+    "revision": "963b1aa7bc61810cd573114e15a9b840",
+    "url": "./static/media/Kollektif-Bold.963b1aa7.otf"
+  },
+  {
+    "revision": "9e393ffabd83a97ea7899343b86fe755",
+    "url": "./static/media/Kollektif-Bold.9e393ffa.ttf"
+  },
+  {
+    "revision": "ce69dac36e640f36c9db36e9e3d13997",
+    "url": "./static/media/Kollektif-Bold.ce69dac3.woff2"
+  },
+  {
+    "revision": "ffbf133601b1fa01905a302f8c84f184",
+    "url": "./static/media/Kollektif-Bold.ffbf1336.eot"
+  },
+  {
+    "revision": "2145719891efb5bed84088b0f2f55897",
+    "url": "./static/media/Kollektif.21457198.eot"
+  },
+  {
+    "revision": "58e525ddc84f543802a214f9b30ac961",
+    "url": "./static/media/Kollektif.58e525dd.otf"
+  },
+  {
+    "revision": "74aa8859707c34d47c19e37c09e846f9",
+    "url": "./static/media/Kollektif.74aa8859.woff"
+  },
+  {
+    "revision": "9b7f0f024690e73de9196ea5ab53eeb9",
+    "url": "./static/media/Kollektif.9b7f0f02.woff2"
+  },
+  {
+    "revision": "df321ebb3980047ab007d7747e4bcf9d",
+    "url": "./static/media/Kollektif.df321ebb.ttf"
+  },
+  {
+    "revision": "f5a8a61af923acae366da305e1ece92f",
+    "url": "./static/media/Kollektif.f5a8a61a.svg"
+  },
+  {
+    "revision": "bf743246a14101eb5813d914470bf8da",
+    "url": "./static/media/LogoCenterHome.bf743246.png"
+  },
+  {
+    "revision": "32e86062f0dcfe3a4170b85ef127bd65",
+    "url": "./static/media/aarushi-shah.32e86062.jpg"
+  },
+  {
+    "revision": "286bcfb5750fc5be1e517aa617db8884",
+    "url": "./static/media/angela-wang.286bcfb5.jpg"
+  },
+  {
+    "revision": "c16b7b309c5fa655d6b2c7bbc9f958a4",
+    "url": "./static/media/claudia-handoyo.c16b7b30.jpg"
+  },
+  {
+    "revision": "622c4ab42312a22069bf62fd692a6996",
+    "url": "./static/media/default-img.622c4ab4.jpg"
+  },
+  {
+    "revision": "8a1e0897a5b9dc4835e6cbe23d7cd3e7",
+    "url": "./static/media/harshi-agrawal.8a1e0897.jpg"
+  },
+  {
+    "revision": "6825c1e87c48b9ee075e1f8a7f2aec8a",
+    "url": "./static/media/home-overlay.6825c1e8.png"
+  },
+  {
+    "revision": "86633e70f790d899def53c85369bffc2",
+    "url": "./static/media/jaida-day.86633e70.jpg"
+  },
+  {
+    "revision": "03fdef7866c68239d3d0c1502f490be1",
+    "url": "./static/media/jaz-gill.03fdef78.jpg"
+  },
+  {
+    "revision": "1dc529b13739fd33b40bc4f04819ddec",
+    "url": "./static/media/jezebel-yangari.1dc529b1.jpg"
+  },
+  {
+    "revision": "25c3c3618a163b07450eea197f107170",
+    "url": "./static/media/judy-liu.25c3c361.JPG"
+  },
+  {
+    "revision": "23338bb2072f71cdea1d900c1e9ce612",
+    "url": "./static/media/kelly-park.23338bb2.jpg"
+  },
+  {
+    "revision": "3e57ebfcf24918fc7d77a87b01643135",
+    "url": "./static/media/kumbh-sans-all-400-normal.3e57ebfc.woff"
+  },
+  {
+    "revision": "d9612b6b4bc46df96cafcb00889a37a9",
+    "url": "./static/media/kumbh-sans-all-700-normal.d9612b6b.woff"
+  },
+  {
+    "revision": "e3653cc32513bd8a508d885acef0c7ef",
+    "url": "./static/media/kumbh-sans-latin-400-normal.e3653cc3.woff2"
+  },
+  {
+    "revision": "57062e46d835c8d428e1374f307eea59",
+    "url": "./static/media/kumbh-sans-latin-700-normal.57062e46.woff2"
+  },
+  {
+    "revision": "4cd876eb86d9b4a55d12c547c3eb155b",
+    "url": "./static/media/kumbh-sans-latin-ext-400-normal.4cd876eb.woff2"
+  },
+  {
+    "revision": "a29f4f6e7785c17df512fdf01258f428",
+    "url": "./static/media/kumbh-sans-latin-ext-700-normal.a29f4f6e.woff2"
+  },
+  {
+    "revision": "db4f5ea67b1f5470f7f2d66b15369223",
+    "url": "./static/media/lavanya-verma.db4f5ea6.jpg"
+  },
+  {
+    "revision": "0e74bcd7b9faf95b7447034f47dcf690",
+    "url": "./static/media/lily-steiner.0e74bcd7.jpg"
+  },
+  {
+    "revision": "2e22bc1136ac2cb2144a0211193b0b61",
+    "url": "./static/media/lindsey-manzano.2e22bc11.jpg"
+  },
+  {
+    "revision": "83ddb90ab7cbcfd856ed8931e60fd6df",
+    "url": "./static/media/navika-budhraja.83ddb90a.JPG"
+  },
+  {
+    "revision": "a5f1e55d263068445d2c82d8dbe1e71a",
+    "url": "./static/media/nora-yinxuan-du.a5f1e55d.jpg"
+  },
+  {
+    "revision": "8f9878008ada2879e83cb898be214f73",
+    "url": "./static/media/photo-four.8f987800.JPG"
+  },
+  {
+    "revision": "16170671c594444e8d69f1070e55fb8a",
+    "url": "./static/media/photo-nine.16170671.JPG"
+  },
+  {
+    "revision": "bd9e52e2f6de3adef79de655ae348a91",
+    "url": "./static/media/photo-one.bd9e52e2.JPG"
+  },
+  {
+    "revision": "10408f03071958fde6ff9871d3f40c61",
+    "url": "./static/media/photo-six.10408f03.JPG"
+  },
+  {
+    "revision": "60592c6a2cfb7ddd6578dcd2286760a6",
+    "url": "./static/media/photo-ten.60592c6a.JPG"
+  },
+  {
+    "revision": "17a62bc3b1658b973dd664d3adf66c1c",
+    "url": "./static/media/photo-two.17a62bc3.JPG"
+  },
+  {
+    "revision": "a491fec196cd34af5551f10cd4d0261c",
+    "url": "./static/media/shreya-velagala.a491fec1.jpg"
+  },
+  {
+    "revision": "d3e65cf347134f32b7fea1b494fd454c",
+    "url": "./static/media/sravya-balasa.d3e65cf3.jpg"
+  },
+  {
+    "revision": "28fa7a9c640684783bea5f5907bb62e0",
+    "url": "./static/media/sydney-wong.28fa7a9c.jpg"
+  },
+  {
+    "revision": "29d2fa1209f5ef4abebf375bad4e0c51",
+    "url": "./static/media/tri-truong.29d2fa12.jpg"
+  },
+  {
+    "revision": "9fa398f453bddb661ed89d3e7a29c67b",
+    "url": "./static/media/vas-sengupta.9fa398f4.jpg"
+  },
+  {
+    "revision": "321ea7779ab2585a8c96abafc9d16c28",
+    "url": "./static/media/vivian-dang.321ea777.jpg"
+  },
+  {
+    "revision": "c6c62971317ee4119fae82aa36c46867",
+    "url": "./static/media/vivian-wu.c6c62971.jpg"
+  },
+  {
+    "revision": "bf743246a14101eb5813d914470bf8da",
+    "url": "./static/media/wic_logo_purple.bf743246.png"
+  }
+]);
