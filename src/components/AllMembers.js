afda8c94d654708edb7de31de499b23583e41850
Aiko Coanaya <aikocoanaya@gmail.com>
2022-10-08 15:09:02 +0100

    update pics

--- src/components/AllMembers.js
+++ src/components/AllMembers.js
@@ -121,123 +121,83 @@ export const all_members = [
     bio: "Hello, I’m Nitya— 3rd year Computer Engineering major at Warren! I love meeting new people and look for new avenues towards self-improvement. I do like to stay in and just watch shows for a couple of hours with a bag of hot cheeto puffs too. A highlight from my summer is hiking at national parks, specifically Yosemite!"
   },
   {
-    picture: require("../static/board-members/aarushi-shah.jpg"),
-    name: "Aarushi Shah",
-    position: "Technical Development Co-Chair",
-    year: "Class of 2022",
+    picture: require("../static/board-members/GingerS.jpg"),
+    name: "Ginger Smith",
+    position: "PR Marketing",
+    year: "Class of 2025",
     linkedin: "",
     instagram: "",
     github: "",
-    bio: "Hi everyone, my name is Aarushi Shah and I am one of the Technical Development Chairs this year! I am a 3rd year Computer Science major in Warren :) In addition to WIC, I am also apart of a dance team on campus called UCSD Raas Ruckus. I love dancing and being apart of a team as it is such a great way to make new friends and relieve some stress from school :)"
+    bio: "Hi I’m Ginger! I’m a second year from Seattle studying CS and French and I’m in ERC! I’m excited about working on the PR team this year :) A highlight from my summer was paddleboarding at night and stargazing on the lake with one of my friends!"
   },
   {
-    picture: require("../static/board-members/lavanya-verma.jpg"),
-    name: "Lavanya Verma",
-    position: "Technical Development Co-Chair",
-    year: "Class of 2023",
-    linkedin: "",
-    instagram: "",
-    github: "",
-    bio: "Hi! I’m Lavanya, the Tech co-Chair for Wic. I’m starting my 3rd year as a Computer Science major at Warren College. This summer I finally visited Harry Potter world, hiked 6 miles at Grand Canyon and kayaked."
-  },
-  {
-    picture: require("../static/board-members/jaida-day.jpg"),
-    name: "Jaida Day",
-    position: "Web Development Chair",
-    year: "Class of 2022",
-    linkedin: "https://www.linkedin.com/in/jaida-day/",
-    instagram: "",
-    github: "https://github.com/aadiaaj",
-    bio: "Hi everyone! My name is Jaida Day and I’m a senior majoring in Math-CS. I’m from Revelle College #firstandfinest and in my free time I like to dance and/or hang with family and friends. Over the summer I interned with PricewaterhouseCoopers, visited a lot of view spots, and hung out with some dolphins!"
-  },
-  {
-    picture: require("../static/board-members/harshi-agrawal.JPG"),
-    name: "Harshi Agrawal",
-    position: "Social Co-Chair",
-    year: "Class of 2022",
-    linkedin: "https://www.linkedin.com/in/harshiagrawal/",
-    instagram: "https://www.instagram.com/harshiagrawal/?hl=en",
-    github: "",
-    bio: "Hey my name is Harshi. I’m a senior majoring in Computer science and minoring in Business in Warren college. I’m super excited to be back in person and hopefully meet more of you! This summer I interned at Qualcomm, spent a lot of time with friends and family, and did some traveling!"
-  },
-  {
-    picture: require("../static/board-members/lily-steiner.JPG"),
-    name: "Lily Steiner",
-    position: "Social Co-Chair",
-    year: "Class of 2023",
-    linkedin: "",
-    instagram: "",
-    github: "",
-    bio: "Hi everyone! My name is Lily and I’m a 3rd year CS Bioinformatics major in Warren. This summer I did research at a lab where I worked on accelerating bioinformatics tools. I spent my free time out with friends, cooking, and traveling! My summer obsession has been animal crossing and collecting vinyl :)"
-  },
-  {
-    picture: require("../static/board-members/nora-yinxuan-du.jpg"),
-    name: "Nora Yinxuan Du",
-    position: "BPC Co-Chair",
-    year: "Class of 2023",
-    linkedin: "",
-    instagram: "",
-    github: "",
-    bio: "My name is Nora Yinxuan Du. I am a junior majoring in CS and I am from Warren College. I moved to the South Bay this summer and had a great experience interning at Salesforce. A fun fact is that I took advantage of volunteering time-off and gave back to the community. The most exciting part for me was volunteering with my fellow interns on a beach cleanup in San Francisco, which took me 1.5 hours on the Caltrain to get there."
-  },
-  {
-    picture: require("../static/board-members/tri-truong.jpg"),
-    name: "Tri Truong",
-    position: "BPC Co-Chair",
-    year: "Class of 2022",
-    linkedin: "",
-    instagram: "",
-    github: "",
-    bio: "Hello! I am Tri Truong and I am a senior majoring in CS at Warren College. Last summer, besides working, I played a lot of basketball, watched a lot of movies, and ate a lot of food with my friends. I also dropped my summer classes so I could have time to play on my PS5, which took me a month to buy because it was always sold out. A fun fact about me is that I stuffed a lot of weights and gym machines in my bedroom because all the gyms were closed during quarantine."
-  },
-  {
-    picture: require("../static/board-members/sravya-balasa.jpg"),
-    name: "Sravya Balasa",
-    position: "Outreach Co-Chair",
-    year: "Class of 2022",
-    linkedin: "",
-    instagram: "",
-    github: "https://github.com/sravyabalasa",
-    bio: "Hi everyone, my name is Sravya Balasa and I'm WIC's Outreach Co-Chair! I'm a 4th year Computer Science major, Design minor, in ERC :) After the stressful past year, I wanted to use this summer to put more time into what I loved. When I wasn't doing my internship at Salesforce, I did many photography shoots, wrote more on my Medium blog, painted, and traveled! I went to Seattle and Kauai and it was so green and beautiful."
-  },
-  {
-    picture: require("../static/board-members/vivian-wu.jpg"),
+    picture: require("../static/board-members/VivianW.jpg"),
     name: "Vivian Wu",
-    position: "Outreach Co-Chair",
+    position: "PR Marketing",
     year: "Class of 2023",
     linkedin: "",
     instagram: "",
     github: "",
-    bio: "Hi! I'm a third year Computer Science major and Cognitive Science minor in Warren! A fun fact about me is that I play Table Tennis for ucsd and for fun. Over the summer, I did a lot of exercising, knitting, and embroidering."
+    bio: "Hi, I’m Vivian! I’m a fourth year CS^2 (comp sci / cog sci) major/minor and I’m in Warren College. Something fun that I did this summer was staying in and exploring Seattle. I was able to meet lots of cool people, eat lots of good food, and visit lots of cool places! I’m looking forward to meeting more cool people and doing cool things in the Fall quarter :)"
   },
   {
-    picture: require("../static/board-members/claudia-handoyo.jpg"),
-    name: "Claudia Handoyo",
-    position: "Public Relations-Publicity Co-Chair",
-    year: "Class of 2022",
-    linkedin: "",
-    instagram: "",
-    github: "",
-    bio: "Hi everyone! My name is Claudia and I’m WIC’s PR-Publicity Chair this year. I’m a 4th year in ERC studying Math-CS. I enjoy cooking, eating, watching movies/dramas/anime, and traveling! A fun fact about me is that I usually order my bubble tea drinks with no sugar and no ice. Looking forward to meeting all of you this year!"
-  },
-  {
-    picture: require("../static/board-members/vivian-dang.jpg"),
+    picture: require("../static/board-members/VivianD.jpg"),
     name: "Vivian Dang",
-    position: "Public Relations-Marketing Co-Chair",
+    position: "PR Publicity",
     year: "Class of 2023",
     linkedin: "",
     instagram: "",
     github: "",
-    bio: "Hi ya’ll! I’m Vivian, this year’s PR-Marketing Chair. I’m a third year majoring in Math-Computer Science (please hit me up if you’re also math-cs) from Revelle College. I like watching Netflix and working out in my free time. Over the summer I interned at General Atomics (with Navika, one of the VPE co-chairs), hiked and ran a lot, and got to spend time with friends and family <3"
+    bio: "Hi everyone! I’m Vivian and I’m a fourth year math-cs major from Revelle college. A highlight from this summer was traveling to SF with my friends and exploring the bay area."
   },
   {
-    picture: require("../static/board-members/2021-2022.jpg"),
-    name: "2021-2022 Board Members",
-    position: "",
-    year: "",
-    linkedin: "https://www.linkedin.com/company/wicucsd/about/",
-    instagram: "https://www.instagram.com/wicucsd/",
+    picture: require("../static/board-members/Shreya.jpeg"),
+    name: "Shreya Velagala",
+    position: "First Year Chair",
+    year: "Class of 2025",
+    linkedin: "",
+    instagram: "",
     github: "",
-    bio: "Feel free to reach out to any of our board members for any questions or advice! We are here to help you and be a resource! Don't forget our social media accounts are linked in the 'Contact Us' section for you to stay updated on events and ask any questions."
+    bio: "Hi, My name is Shreya, I am going to be a sophomore at UCSD this year majoring in Computer Science at Warren College. This summer I went to Canada for the first time and did some touristy things which was very exciting. Some fall quarter activities that I am looking forward to this year are the warren first week events."
+  },
+  {
+    picture: require("../static/board-members/kelly.jpg"),
+    name: "Kelly Park",
+    position: "BPC Co-Chair",
+    year: "Class of 2024",
+    linkedin: "",
+    instagram: "",
+    github: "",
+    bio: "Hello hello !! My name is Kelly, and I’m a 3rd year in Warren majoring in Data Science and minoring in Korean Studies! I’m super excited to be serving WIC as BPC Co-Chair this year! One thing I’m looking forward to for fall quarter is meeting new people through WIC and other orgs I’m involved in."
+  },
+  {
+    picture: require("../static/board-members/melody.jpg"),
+    name: "Melody Gill",
+    position: "BPC Co-Chair",
+    year: "Class of 2025",
+    linkedin: "",
+    instagram: "",
+    github: "",
+    bio: "Hello! I'm Melody, and I am a second-year computer science major in Sixth College. A highlight from my summer was spending a weekend in San Francisco with my friends and getting to be tourists for a couple days. I'm excited to be BPC Co-Chair this year!"
+  },
+  {
+    picture: require("../static/board-members/Helena.jpg"),
+    name: "Helena Hundhausen",
+    position: "Outreach Co-Chair",
+    year: "Class of 2025",
+    linkedin: "",
+    instagram: "",
+    github: "",
+    bio: "Hello I’m Helena, a second-year warren student majoring in computer science. This fall quarter I’m really looking forward to getting Blue Bowl on campus and meeting new people part of the WIC community! A highlight of my summer was visiting my best friends from high school and going to a Lumineers concert (even though our car got towed, lol)"
+  },
+  {
+    picture: require("../static/board-members/lina.jpg"),
+    name: "Lina Battikha",
+    position: "Outreach Co-Chair",
+    year: "Class of 2025",
+    linkedin: "",
+    instagram: "",
+    github: "",
+    bio: "Hi I’m Lina! I'm one of the co-outreach chairs for this upcoming school year! I'm a sophomore in Warren College, majoring in Data Science. One of my summer highlights is a mini trip I took to Palm Dessert with friends. Very excited to meet new people through WIC!! :)"
   },
 ];
